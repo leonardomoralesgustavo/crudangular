fbea71e874d121e17c6cb6a2b32d41bb5ed16119
Gustavo <leonardomoralesgustavo@gmail.com>
2021-10-21 14:11:57 -0500

    update

--- src/app/componentes/agregar-empleado/agregar-empleado.component.ts
+++ src/app/componentes/agregar-empleado/agregar-empleado.component.ts
@@ -26,7 +26,6 @@ export class AgregarEmpleadoComponent implements OnInit {
   }
 
   enviarDatos():any{
-    console.log("Me presionaste");
     console.log(this.formularioDeEmpleados.value);
     this.crudService.AgregarEmpleado(this.formularioDeEmpleados.value).subscribe(respuesta=>{
       this.ruteador.navigateByUrl('/listar-empleado');
